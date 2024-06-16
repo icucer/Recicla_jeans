087e62f541fcc308c63181856f86f048cdb3f59d
Ion <ioncucer@gmail.com>
2024-06-16 13:22:16 -0400

    Implimentacion de la clase utilidad

--- src/cl/praxis/tienda/Utilidad.java
+++ src/cl/praxis/tienda/Utilidad.java
@@ -3,6 +3,20 @@ package cl.praxis.tienda;
 import java.util.InputMismatchException;
 
 public class Utilidad {
+    // Método para limpiar la pantalla alternativo
+    public static void limpiarPantalla() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
 
-
+    // Método para realizar una pausa en la ejecución
+    public static void esperar(int segundos) {
+        try {
+            Thread.sleep(segundos * 1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
 }
